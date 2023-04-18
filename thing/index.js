78978f2820a19cbbcb81939147b674610bd96500
Angelo Fernandes <afbo2003@gmail.com>
2023-04-18 10:28:24 -0300

    adjust thing script

--- thing/index.js
+++ thing/index.js
@@ -2,18 +2,18 @@ import mqtt from 'mqtt';
 
 const client = mqtt.connect('mqtt://localhost:1883');
 
+let count = 1;
+
 client.on('connect', () => {
   console.log('conectado ao broker');
-  getVolume();
+  setInterval(() => {
+    const message = { volume: count };
+
+    client.publish('casa', JSON.stringify(message));
+    count++;
+  }, 1000);
 });
 
 client.on('error', (error) => {
   console.log(error);
 });
-
-const getVolume = () => {
-  for (let index = 0; index <= 10; index++) {
-    const message = { volume: index + 10 };
-    client.publish('casa', JSON.stringify(message));
-  }
-};
