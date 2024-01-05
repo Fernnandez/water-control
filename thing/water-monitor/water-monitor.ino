7f6579032d200ab940ee5eb5e85e7e0882f93f7b
Angelo Fernandes <67211421+Fernnandez@users.noreply.github.com>
2024-01-05 13:28:15 -0300

    adjust ino code

--- thing/water-monitor/water-monitor.ino
+++ thing/water-monitor/water-monitor.ino
@@ -2,70 +2,37 @@
 #include <WiFiUdp.h>
 #include <NTPClient.h>
 #include <PubSubClient.h>
-#include <WiFiClientSecure.h>
+#include <ESP8266HTTPClient.h>
 
 #define TrigPin 12
 #define EchoPin 14
 
 /********** WiFi Connection Details **********/
-const char* ssid = "viva 3G";
-const char* password = "12345678";
+const char* ssid = "DTEL_ALBERES_2.4";
+const char* password = "#MFDO1983@";
 
 /********** MQTT Broker Connection Details **********/
-const char* mqtt_server = "f9ca89e3f5df46e6a4b2e122b2d56a3c.s2.eu.hivemq.cloud"; // public MQTT broker
-const char* mqtt_username = "username";
-const char* mqtt_password = "@MQTT-password1";
-const int mqtt_port = 8883;
-
-/****** root certificate *********/
-
-static const char *root_ca PROGMEM = R"EOF(
------BEGIN CERTIFICATE-----
-MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
-TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
-cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4
-WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu
-ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY
-MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc
-h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+
-0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U
-A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW
-T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH
-B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC
-B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv
-KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn
-OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn
-jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw
-qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI
-rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV
-HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq
-hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL
-ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ
-3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK
-NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5
-ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur
-TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC
-jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc
-oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
-4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA
-mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
-emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
------END CERTIFICATE-----
-)EOF";
-
+const char* mqtt_server = "broker.hivemq.com"; // public MQTT broker
+const char* mqtt_username = "your_mqtt_client_username";
+const char* mqtt_password = "your_mqtt_client_password";
+const int mqtt_port = 1883; // Porta padrão para MQTT sem TLS
 
 /********** NTP Client Setup **********/
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org");
 
 /********** MQTT Client Setup **********/
-WiFiClientSecure espClient;
+WiFiClient espClient; // Alteração para WiFiClient ao invés de WiFiClientSecure
+HTTPClient http;
 PubSubClient client(espClient);
 
 /********** Global State Variables **********/
-const int updateInterval = 10000; // Intervalo de atualização em milissegundos
-int previousDistance = 0;
+int updateInterval = 10000;
 int previousBattery = 100;
+const int pinA0 = A0;
+const float Vin = 5.0;  // Voltagem de entrada que você quer medir (por exemplo, 5V)
+const float Vref = 3.3; // Tensão de referência do ESP8266 (3.3V para o ESP8266)
+
 
 struct Device {
   String macAddress;
@@ -95,24 +62,91 @@ void setupWiFi() {
   Serial.println(WiFi.localIP());
 }
 
+float readDistanceSensor() {
+  digitalWrite(TrigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TrigPin, HIGH);
+  delayMicroseconds(10);
+  
+  float durationindigit = pulseIn(EchoPin, HIGH);
+  return (durationindigit / 2) / 29.1;
+}
 
-/************* Connect to MQTT Broker ***********/
+float readVoltageSensor() {
+  int sensorValue = analogRead(pinA0);
+  float tensao = sensorValue * (3.3 / 1023.0);
+  return (sensorValue / 1023.0) * Vref;
+}
+
+void makeHttpRequest(const char* host, const char* endpoint) {
+
+  Serial.print("Conectando a: ");
+  Serial.println(host);
+
+  if (!http.begin(espClient, "http://" + String(host) + endpoint)) {
+    Serial.println("Falha ao iniciar a conexão");
+    return;
+  }
+
+  int httpCode = http.GET();
+
+  if (httpCode < 0) {
+    Serial.println("Erro na requisição - " + String(httpCode));
+    http.end();
+    return;
+  }
+
+  if (httpCode != HTTP_CODE_OK) {
+    Serial.println("Erro na resposta do servidor");
+    http.end();
+    return;
+  }
+
+  String payload = http.getString();
+
+  http.end();
+
+  Serial.println("##[RESULT]## ==> " + payload);
+}
+
+void monitor(){
+  int currentHour = timeClient.getHours();
+  analyser(currentHour);
+}
+
+void analyser(int hour){
+  // Verifica se a hora está entre meia noite (0h) e cinco da manhã (5h)
+  if (hour >= 0 && hour < 5) {
+    Serial.println("A hora está entre meia noite e cinco da manhã.");
+    // Faça o que precisa ser feito durante este intervalo de tempo
+    // Pode ser retornado true aqui se necessário
+    // executor(<valor de 1 hora>);
+  } else {
+    //  makeHttpRequest("192.168.18.220:3001", "/devices");
+    //chamar requisição HTTP para aquamon e verificar se tem adaptação por volume
+  }
+}
+
+void executor(int sleepTime){
+  Serial.println(sleepTime);
+  updateInterval = sleepTime;
+}
+
 void reconnect() {
-  // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
-    String clientId = "ESP8266Client-";   // Create a random client ID
+    String clientId = "ESP8266Client-";
     clientId += String(random(0xffff), HEX);
-    // Attempt to connect
+
     if (client.connect(clientId.c_str(), mqtt_username, mqtt_password)) {
       Serial.println("connected");
 
-      client.subscribe("led_state");   // subscribe the topics here
+      client.subscribe("led_state"); // subscribe the topics here
 
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");   // Wait 5 seconds before retrying
+      Serial.println(" try again in 5 seconds");
       delay(5000);
     }
   }
@@ -126,63 +160,52 @@ void setup() {
 
   setupWiFi();
 
-  #ifdef ESP8266
-    espClient.setInsecure();
-  #else
-    espClient.setCACert(root_ca);      // enable this line and the the "certificate" code for secure connection
-  #endif
-
   client.setServer(mqtt_server, mqtt_port);
   
   timeClient.begin();
-  timeClient.setTimeOffset(-10800); // Ajuste de fuso horário (em segundos)
+  timeClient.setTimeOffset(-10800);
 }
 
 void loop() {
-  if (!client.connected()) reconnect(); // check if client is connected
+  if (!client.connected()) reconnect();
   client.loop();
 
   timeClient.update();
   
   unsigned long timestamp = timeClient.getEpochTime();
 
-  digitalWrite(LED_BUILTIN, LOW); // Liga o LED
-
-  digitalWrite(TrigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TrigPin, HIGH);
-  delayMicroseconds(10);
-
-  float durationindigit = pulseIn(EchoPin, HIGH);
-
-  float distance = (durationindigit/2) / 29.1;
-
-  // Simulação da alteração da distância do sensor
-  int diffDistance = random(-10, 11);
-  int currentDistance = constrain(previousDistance + diffDistance, 0, device.height);
+  int currentHour = timeClient.getHours();
 
   if(previousBattery < 25){
     previousBattery = 100;
   }
 
-  // Simulação da diminuição da bateria
   int currentBattery = max(previousBattery - 1, 0);
 
-  // Criação da mensagem MQTT com a distância atual e nível de bateria
+  float distance = readDistanceSensor();
+  float voltage = readVoltageSensor();
+
+  Serial.print("\t Voltagem lida: ");
+  Serial.print(voltage);
+
+  Serial.print("\t Distancia lida: ");
+  Serial.print(distance);
+
   String macAddress = device.macAddress;
   String message = "{\"distance\": " + String(distance) + ", \"battery\": " + String(currentBattery) + ", \"timestamp\": " + String(timestamp) + "}";
 
-  Serial.print("Enviando mensagem para o tópico: ");
+  Serial.print("\t Enviando mensagem para o tópico: ");
   Serial.println(macAddress);
   Serial.print("Mensagem: ");
   Serial.println(message);
 
   client.publish(macAddress.c_str(), message.c_str());
-
-  previousDistance = currentDistance;
+  
   previousBattery = currentBattery;
 
-  digitalWrite(LED_BUILTIN, HIGH); // Desliga o LED
+  monitor();
+
+  digitalWrite(LED_BUILTIN, HIGH);
 
   delay(updateInterval);
 }
